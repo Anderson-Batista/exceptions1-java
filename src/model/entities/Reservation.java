066a6d8e646e07daaf2cac6bf637e35314028434
Anderson Batista Ferreira da Silva <andersonbfs15@gmail.com>
2021-01-20 11:09:01 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -33,17 +33,25 @@ public class Reservation {
 	public Date getCheckOut() {
 		return checkOut;
 	}
-	
+
 	public long duration() {
 		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update must be future dates";
+		}
+		if (!checkOut.after(checkIn)) {
+			return "Check-out date must be after check-in date";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
-
+	
 	@Override
 	public String toString() {
 		return "Room "
@@ -54,9 +62,7 @@ public class Reservation {
 			+ sdf.format(checkOut)
 			+ ", "
 			+ duration()
-			+ " nigths";
+			+ " nights";
 	}
-	
-	
 }
 
